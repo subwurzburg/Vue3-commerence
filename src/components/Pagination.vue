a78156f136b518048b297883144c74bbd5c45fb6
subwurzburg24 <you@example.com>
2022-01-24 20:55:11 +0800

    分頁的啟用(前一頁、後一頁)、active

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,18 +1,18 @@
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
+      <li class="page-item" :class="pages.has_pre ? '' : 'disabled'">
+        <a class="page-link" href="#" aria-label="Previous" @click.prevent="updatePage(pages.current_page-1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
-      <li class="page-item" v-for="(item) in pages.total_pages" :key="item">
+      <li class="page-item" v-for="(item) in pages.total_pages" :key="item" :class="item==pages.current_page ? 'active' : '' ">
         <a class="page-link" href="#" @click.prevent="updatePage(item)">
           {{ item }}
         </a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
+      <li class="page-item" :class="pages.has_next ? '' : 'disabled'">
+        <a class="page-link" href="#" aria-label="Next" @click.prevent="updatePage(pages.current_page+1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
@@ -25,6 +25,7 @@ export default {
   props: ['pages'],
   methods: {
     updatePage (page) {
+      console.log(page)
       this.$emit('emitPages', page)
     }
   }
